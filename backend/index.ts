e550183fbbab8df63cff12b20a677ef57b04dad8
KovacsLevente199 <kovacslevente199@gmail.com>
2022-09-10 18:58:14 +0200

    Added endpoint for testing

--- backend/index.ts
+++ backend/index.ts
@@ -4,3 +4,10 @@ const PORT = process.env.port || 3001;
 const app = express();
 
 app.listen(PORT,()=>{console.log(`Server listening on port ${PORT}`)});
+
+function testfunction():void
+{
+  console.log("Get request recived.");
+}
+
+app.get("/api",testfunction);
