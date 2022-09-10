39cb6a52d2979e6983f13892b70da9aa16915906
Bence Bakos <58910140+bencebakos3d@users.noreply.github.com>
2022-09-10 19:23:50 +0200

    Tsconfig etc.. added

--- backend/index.ts
+++ backend/index.ts
@@ -1,13 +1,14 @@
-const express = require("express");
+const express = require('express');
 
 const PORT = process.env.port || 3001;
 const app = express();
 
-app.listen(PORT,()=>{console.log(`Server listening on port ${PORT}`)});
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
 
-function testfunction():void
-{
-  console.log("Get request recived.");
+function testfunction(): void {
+  console.log('Get request recived.');
 }
 
-app.get("/api",testfunction);
+app.get('/api', testfunction);
