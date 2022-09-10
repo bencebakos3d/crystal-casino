8a5a740c927a9db3805b307b9576078806cd8df4
Bence Bakos <58910140+bencebakos3d@users.noreply.github.com>
2022-09-10 17:34:06 +0200

    Created Header and functionality

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
+import headerImg from './images/diamond-logo.png';
 
 export default function Header() {
   return (
     <div className="header-wrapper">
-      <div>
-        <div></div>
-        <h2>Ez itt a header</h2>
+      <div className="header-logo">
+        <img src={headerImg} alt="" />
+      </div>
+      <div className="header">
+        <Link to="/" className="menu-button">
+          HOME
+        </Link>
+        <Link to="/" className="menu-button">
+          ROULETTE
+        </Link>
+        <Link to="/" className="menu-button">
+          SLOTS
+        </Link>
+        <Link to="/" className="menu-button">
+          BLACKJACK
+        </Link>
       </div>
     </div>
   );
