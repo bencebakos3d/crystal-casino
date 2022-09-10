23624a4e6f360fac5556b36ae644686fae45c02a
Bence Bakos <58910140+bencebakos3d@users.noreply.github.com>
2022-09-10 17:02:32 +0200

    Added ReactRouter and index component

---
+++ src/pages/Index/Index.tsx
@@ -0,0 +1,13 @@
+export default function Index() {
+  // Ide jon a useState es useEffect kod meg mindenfele valtozo
+  //
+
+  return (
+    <div className="page-wrapper">
+      <div>
+        <div></div>
+        <h2>EXPERIENCE EXCELLENCE</h2>
+      </div>
+    </div>
+  );
+}
