24436e54fad8f63b71947da8fdeefa8e821583b6
Bence Bakos <58910140+bencebakos3d@users.noreply.github.com>
2022-09-10 19:02:16 +0200

    Deleted node modules from backend

--- frontend/src/pages/Index/Index.tsx
+++ frontend/src/pages/Index/Index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import './Index.css';
+import bgImg from './images/diamond-bg.png';
 
 export default function Index() {
   // Ide jon a useState es useEffect kod meg mindenfele valtozo
@@ -6,12 +8,15 @@ export default function Index() {
 
   return (
     <div className="page-wrapper">
-      <div>
-        <div className="divider"></div>
-        <h2>EXPERIENCE EXCELLENCE</h2>
+      <img src={bgImg} alt="Diamond Casino" className="bg-img" />
+      <div className="textbox">
+        <div className="experience-wrapper">
+          <div className="divider"></div>
+          <h2>EXPERIENCE EXCELLENCE</h2>
+        </div>
+        <h1>A LUXURY CASINO & RESORT IN THE HEART OF VINEWOOD</h1>
+        <p>You've heard the rumors. You want to believe them, and there's only one way to find out. Abandon your preconceptions. Let go of your inhibitions, your doubts, your sense of proportion, vour credit rating. The rumors didn't begin to do it justice. Welcome to The Diamond. Grand Opening July 23.</p>
       </div>
-      <h1>A LUXURY CASINO & RESORT IN THE HEART OF VINEWOOD</h1>
-      <div></div>
     </div>
   );
 }
