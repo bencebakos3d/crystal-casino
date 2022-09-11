4679b63959a65928fded7b7847810dd1c49339a6
Bence Bakos <bencemail1@gmail.com>
2022-09-11 20:16:07 +0200

    Roulette ball img added

--- frontend/src/pages/Roulette/Roulette.tsx
+++ frontend/src/pages/Roulette/Roulette.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Roulette.css';
 import rouletteWheelImg from './images/roulette-wheel.png';
+import rouletteBallImg from './images/roulette-ball.png';
 
 export default function Roulette() {
   function spinRoulette() {}
@@ -8,7 +9,10 @@ export default function Roulette() {
   return (
     <div className="page-wrapper">
       <div className="roulette-table-wrapper">
-        <img src={rouletteWheelImg} alt="" className="roulette-wheel" />
+        <div className="roulette-wheel-wrapper">
+          <img src={rouletteWheelImg} alt="" className="roulette-wheel" />
+          <img src={rouletteBallImg} alt="" className="roulette-ball" />
+        </div>
         <div className="roulette-table"></div>
       </div>
     </div>
