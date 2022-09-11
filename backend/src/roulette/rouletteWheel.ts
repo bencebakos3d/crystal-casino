ed00df8e7a3320b74892ab6897d65a2650fabac3
Kovacs Levente <kovacslevente199@gmail.com>
2022-09-11 12:51:21 +0200

    Added roulette wheel for testing

--- backend/src/roulette/rouletteWheel.ts
+++ backend/src/roulette/rouletteWheel.ts
@@ -1,5 +1,17 @@
 import {RouletteFields} from "./rouletteFields";
+//https://www.youtube.com/watch?v=WIIf3WaO5x4
+//https://www.grc.com/otg/uheprng.htm?fbclid=IwAR1xL4H59C4z_pWLrIfPpy8B1tDpcD_lGVHBBZgTEXiuc029RXs3zuxZTnY
 
+
+/*
+    1 szelet 9.7 fok
+    1 teljes rulett kör 360 fok
+    1 pörgetés = lefele kerekít(pörgettett fok / 1 szelet)
+    pl.: 1 pörgetés = lekerekít(38/9.7) = 3.91 --most lekerekítem
+    1 pörgetés = 3 szelet
+    pozició = jelenlegi elem +3 ;
+    return rulettefields[pozició]
+*/
 class RouletteWheel{
     public constructor(){
         console.log(RouletteFields[0]);
