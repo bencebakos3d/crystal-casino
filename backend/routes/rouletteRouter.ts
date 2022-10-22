930186e058455553d7ee887d4ffc991db467b896
Bence Bakos <58910140+bencebakos3d@users.noreply.github.com>
2022-10-23 00:44:04 +0200

    Session id fixed

--- backend/routes/rouletteRouter.ts
+++ backend/routes/rouletteRouter.ts
@@ -7,11 +7,10 @@ export function setUpRouletteRoutes() {
     const numbers = req.body.numbers;
     const bets = req.body.bets; //hozzáadni a felhasználó nevet
     console.log(req.cookies);
-    let finalResult:any;
-    if(req.cookies.length != 0){
-      finalResult = await playRoulette(numbers, bets, req.cookies.SessionID, 'test')
-    }
-    else{
+    let finalResult: any;
+    if (req.cookies.length != 0) {
+      finalResult = await playRoulette(numbers, bets, req.cookies.SessionID, 'test');
+    } else {
       finalResult = await playRoulette(numbers, bets, req.session.id, 'test');
     }
     console.log(finalResult);
