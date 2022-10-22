9c15949bdc70cd599522fa2d693c56b009a2c640
Bence Bakos <58910140+bencebakos3d@users.noreply.github.com>
2022-10-22 19:25:51 +0200

    Added index.html serving to backend

--- backend/index.ts
+++ backend/index.ts
@@ -2,18 +2,19 @@ const cors = require('cors');
 import { setUpRouletteRoutes } from './routes/rouletteRouter';
 import { initSessions, setUpSessionHandlers } from './sessions/session';
 import { dbHandler } from './database/databaseManager';
-
+const path = require('path');
 const express = require('express');
 const cookieParser = require('cookie-parser');
+
 const PORT = process.env.port || 3001;
 export const app = express();
 
-app.use(
-  cors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     credentials: true,
+//   })
+// );
 app.use(express.json());
 app.use(cookieParser());
 
@@ -23,16 +24,23 @@ app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
 
-let time = (1*60)*1000;
-setInterval(function(){dbHandler.clearExpiredRecord()},time)
-app.get('/setcookie',async (req: any, res: any) => {
+let time = 1 * 60 * 1000;
+setInterval(function () {
+  dbHandler.clearExpiredRecord();
+}, time);
+app.get('/setcookie', async (req: any, res: any) => {
   await dbHandler.queryPlayer(req.session.id); //irni contains függvényt a dbbe
   res.cookie(`SessionID`, req.session.id);
   res.send('Cookie have been saved successfully');
 });
 
+app.use(express.static(path.join(__dirname, 'build')));
 
+app.get('/', function (req: any, res: any) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(9000);
 
 setUpSessionHandlers();
 setUpRouletteRoutes();
-
