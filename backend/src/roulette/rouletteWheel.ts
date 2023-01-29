9be7bfe21233f2c523905f790e382127bd65c6c4
Bence Bakos <58910140+bencebakos3d@users.noreply.github.com>
2023-01-29 16:46:58 +0100

    Removed placeholder number generation from backend

--- backend/src/roulette/rouletteWheel.ts
+++ backend/src/roulette/rouletteWheel.ts
@@ -1,45 +1,35 @@
-import {RouletteFields} from "./rouletteFields";
-import {Player} from '../player/player';
+import { RouletteFields } from './rouletteFields';
+import { Player } from '../player/player';
 
-function generateNumbers():number{
-    //return Math.floor(Math.random() * 36) + 0;
-    return 10;
+function generateNumbers(): number {
+  return Math.floor(Math.random() * 36) + 0;
 }
-export class RouletteWheel{   
-    public spin(gamePlayer:Player){
-        
-        let finalIndex:number = generateNumbers();
-        const winnerNumber = parseInt(RouletteFields.field(finalIndex)[0]);
-        let total = 0;
+export class RouletteWheel {
+  public spin(gamePlayer: Player) {
+    let finalIndex: number = generateNumbers();
+    const winnerNumber = parseInt(RouletteFields.field(finalIndex)[0]);
+    let total = 0;
 
-        //Debug code begins
-        gamePlayer.setNumbers([[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]]);
-        gamePlayer.setBets([1]);
-        console.log(process.env.SECRETS);
-        //Debug code ends
-
-        
-        if(gamePlayer.getBets().length == 0){
-            console.log("error no bets given!");
-        }
-
-        for(let i=0;i<gamePlayer.getNumbers().length;++i){
-            if(gamePlayer.getNumbers()[i].includes(winnerNumber)){
-                total += gamePlayer.getBets()[i]*(36/gamePlayer.getNumbers()[i].length);
-                gamePlayer.decreaseBalance(gamePlayer.getBets()[i]);
-                gamePlayer.increaseBalance(total);
-            }
-            else{
-                gamePlayer.decreaseBalance(gamePlayer.getBets()[i]);
-            }
-        }
-        
-        let responseObject = {
-            balance: gamePlayer.getBalance(),
-            prize: total,
-            winnerNumber: RouletteFields.field(winnerNumber)[0]
-        }
-
-        return responseObject;
+    if (gamePlayer.getBets().length == 0) {
+      console.log('error no bets given!');
     }
+
+    for (let i = 0; i < gamePlayer.getNumbers().length; ++i) {
+      if (gamePlayer.getNumbers()[i].includes(winnerNumber)) {
+        total += gamePlayer.getBets()[i] * (36 / gamePlayer.getNumbers()[i].length);
+        gamePlayer.decreaseBalance(gamePlayer.getBets()[i]);
+        gamePlayer.increaseBalance(total);
+      } else {
+        gamePlayer.decreaseBalance(gamePlayer.getBets()[i]);
+      }
+    }
+
+    let responseObject = {
+      balance: gamePlayer.getBalance(),
+      prize: total,
+      winnerNumber: RouletteFields.field(winnerNumber)[0],
+    };
+
+    return responseObject;
+  }
 }
