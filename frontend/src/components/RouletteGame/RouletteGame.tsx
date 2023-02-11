ee304ca5683842ac7f108493ab3a5420223f905f
Bence Bakos <58910140+bencebakos3d@users.noreply.github.com>
2023-02-11 18:12:47 +0100

    Fixed roulette wheel and table colors

--- frontend/src/components/RouletteGame/RouletteGame.tsx
+++ frontend/src/components/RouletteGame/RouletteGame.tsx
@@ -77,6 +77,8 @@ export default function RouletteGame() {
     console.log('betting ' + value);
   };
   const RouletteFields = [0, 32, 15, 19, 4, 21, 2, 25, 17, 34, 6, 27, 13, 36, 11, 30, 8, 23, 10, 5, 24, 16, 33, 1, 20, 14, 31, 9, 22, 18, 29, 7, 28, 12, 35, 3, 26];
+  const RouletteReds = [32, 19, 21, 25, 34, 27, 36, 30, 23, 5, 16, 1, 14, 9, 18, 7, 12, 3];
+  const RouletteBlacks = [15, 4, 2, 17, 6, 13, 11, 8, 10, 24, 33, 20, 31, 22, 29, 28, 35, 26];
 
   // Sends chosen numbers and bets to the backend, returns the random number and the amount of money the player has
 
@@ -116,7 +118,7 @@ export default function RouletteGame() {
 
         for (let i = 0; i < RouletteFields.length; i++) {
           if (returnedNumber == RouletteFields[i]) {
-            let rouletteAngle = 10 * i - 5;
+            let rouletteAngle = (360 / 37) * i;
             spinRoulette(rouletteAngle);
             setTimeout(() => {
               if (prize > 0) {
@@ -177,7 +179,7 @@ export default function RouletteGame() {
 
   const pastItems = pastNumbers.map((number) => (
     <div className="pastnumber-wrapper">
-      <div>{number}</div>
+      <div className={`${number === 0 ? 'pastnumber-green' : RouletteReds.includes(number) ? 'pastnumber-red' : 'pastnumber-black'}`}>{number}</div>
     </div>
   ));
 
@@ -393,8 +395,8 @@ export default function RouletteGame() {
                   <div className="roulette-table-row-thirdHalves">
                     <div onClick={(event) => betOnNumber(event, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18])}></div>
                     <div onClick={(event) => betOnNumber(event, [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36])}></div>
-                    <div onClick={(event) => betOnNumber(event, [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35])}></div>
-                    <div onClick={(event) => betOnNumber(event, [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36])}></div>
+                    <div onClick={(event) => betOnNumber(event, [32, 19, 21, 25, 34, 27, 36, 30, 23, 5, 16, 1, 14, 9, 18, 7, 12, 3])}></div>
+                    <div onClick={(event) => betOnNumber(event, [15, 4, 2, 17, 6, 13, 11, 8, 10, 24, 33, 20, 31, 22, 29, 28, 35, 26])}></div>
                     <div onClick={(event) => betOnNumber(event, [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35])}></div>
                     <div onClick={(event) => betOnNumber(event, [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36])}></div>
                   </div>
